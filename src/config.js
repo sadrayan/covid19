b65ee04975db81b327015d97516a2ad54554feeb
sadra <sadrayan@gmail.com>
2020-03-21 22:54:13 -0400

    configs updates

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const hostApi = process.env.NODE_ENV === "development" ? "http://localhost" : "https://flatlogic-node-backend.herokuapp.com";
+const hostApi = process.env.NODE_ENV === "development" ? "http://localhost" : "https://www.covid19watch.info";
 const portApi = process.env.NODE_ENV === "development" ? 8080 : "";
 const baseURLApi = `${hostApi}${portApi ? `:${portApi}` : ``}`;
 
@@ -6,7 +6,7 @@ export default {
   hostApi,
   portApi,
   baseURLApi,
-  remote: "https://flatlogic-node-backend.herokuapp.com",
+  remote: "https://www.covid19watch.info",
   isBackend: process.env.REACT_APP_BACKEND,
   auth: {
     email: 'admin@flatlogic.com',
