17d8d08f307d5a8ccbc46da34c15213399858ad8
sadra <sadrayan@gmail.com>
2020-03-29 18:32:09 -0400

    chart styles

---
+++ src/pages/widgets/components/charts/ChartStyles.js
@@ -0,0 +1,24 @@
+ const statsStyles = {
+    "confirmed": {
+        backgroundColor: "rgba(255,180,0,1)",
+        backgroundColorLighter: "rgba(255,180,0,0.8)",
+        backgroundColorFade: "rgba(255,180,0,0)",
+        borderColor: "rgb(255,180,0)",
+        rgb: '#ffb400'
+    },
+    "recovered": {
+        backgroundColor: "rgba(23,198,113,1)",
+        backgroundColorLighter: "rgba(23,198,113,0.8)",
+        backgroundColorFade: "rgba(23,198,113,0)",
+        borderColor: "rgb(23,198,113)",
+        rgb: '#14b265'
+    },
+    "death": {
+        backgroundColor: "rgba(255,65,105,1)",
+        backgroundColorLighter: "rgba(255,65,105,0.8)",
+        backgroundColorFade: "rgba(255,65,105,0)",
+        borderColor: "rgb(255,65,105)",
+        rgb: '#ff4169'
+    }
+}
+export default statsStyles
