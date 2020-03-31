b55fd12bb17bc6995cfe304a2a4609e4b3b1ba84
sadra <sadrayan@gmail.com>
2020-03-30 23:34:30 -0400

    lambda function clean up and amplify auth removed

--- amplify/backend/function/covidfunction/src/app.js
+++ amplify/backend/function/covidfunction/src/app.js
@@ -1,3 +1,10 @@
+/* Amplify Params - DO NOT EDIT
+You can access the following resource attributes as environment variables from your Lambda function
+var environment = process.env.ENV
+var region = process.env.REGION
+
+Amplify Params - DO NOT EDIT */
+
 const DBHelper = require('./dbHelper') 
 
 var express = require('express')
