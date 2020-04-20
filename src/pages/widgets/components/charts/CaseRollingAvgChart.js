10ba7fa2c04e2ff303b9e4a8d84ecbf676538bb1
sadra <sadrayan@gmail.com>
2020-04-19 22:47:06 -0400

    rolling avg component

---
+++ src/pages/widgets/components/charts/CaseRollingAvgChart.js
@@ -0,0 +1,201 @@
+import React from 'react';
+import { Row, Col, FormGroup, Label, Input } from 'reactstrap';
+import Widget from "../../../../components/Widget/Widget";
+import HighchartsReact from 'highcharts-react-official'
+// import statsStyles from './ChartStyles'
+import { API } from 'aws-amplify'
+// import { ma, } from 'moving-averages'
+// const moment = require('moment')
+var nf = new Intl.NumberFormat();
+
+export default class HasCurveFlatten extends React.PureComponent {
+
+
+  constructor(props) {
+    super(props);
+    this.chartRef = React.createRef();
+    this.state = {
+      rollingAvgType: 'confirmed',
+      caseTypeThreshold: {
+        confirmed: 100,
+        active: 100,
+        recovered: 100,
+        deaths: 5
+      }
+    }
+  }
+
+  async componentDidMount() {
+    let result = await API.get('covidapi', '/casePoint/overviewRollingAvgStats')
+    this.setState({ countryCaseRollingAvg: result.body })
+    this.getStatData();
+  }
+
+  /**
+   * analysis done on Active cases and the moving average for trend
+   */
+  getStatData() {
+    const { countryCaseRollingAvg } = this.state
+    let series = []
+    let labels = []
+
+
+    for (let country in countryCaseRollingAvg) {
+      // console.log(country, this.state.rollingAvgType, countryCaseRollingAvg[country])
+      let data = countryCaseRollingAvg[country][this.state.rollingAvgType]
+      data = data.map(el => parseInt(el))
+      data = data.filter(el => el >= this.state.caseTypeThreshold[this.state.rollingAvgType])
+      console.log(country, this.state.rollingAvgType, data)
+      series.push({
+        name: country,
+        type: 'spline',
+        data: data,
+        caseType: this.state.rollingAvgType,
+        tooltip: {
+          valueSuffix: ' mm'
+        },
+        // threshold: this.state.caseTypeThreshold[this.state.rollingAvgType]
+      })
+      labels.push({
+        point: {
+          xAxis: 0,
+          yAxis: 0,
+          x: data.length,
+          y: data[data.length - 1]
+        },
+        text: country
+      })
+    }
+
+    console.log(labels)
+
+    let chartOptions = {
+      credits: {
+        enabled: false
+      },
+      chart: {
+        height: 350,
+        backgroundColor: 'rgba(0,0,0,0)',
+      },
+      title: false,
+      exporting: {
+        enabled: false
+      },
+      subtitle: {
+        text: '7-day rolling average',
+        style: {
+          color: '#595d78'
+        }
+      },
+      legend: {
+        verticalAlign: 'bottom',
+        itemStyle: {
+          color: "rgba(244, 244, 245, 0.9)"
+        },
+        itemHoverStyle: {
+          color: "rgba(244, 244, 245, 0.6)"
+        }
+      },
+      yAxis: {
+        title: false,
+        type: 'logarithmic',
+        labels: {
+          style: {
+            color: "#ffffff"
+          }
+        }
+      },
+      xAxis: {
+        tickInterval: 5,
+        labels: {
+          overflow: 'justify',
+          style: {
+            color: "#ffffff"
+          }
+        }
+      },
+      annotations: [{
+        labels: labels,
+        labelOptions: {
+          x: 40, y: -10
+          // shape: 'connector',
+          // align: 'right',
+          // justify: false,
+          // crop: true,
+          // style: {
+          //   fontSize: '0.8em',
+          //   textOutline: '1px white'
+          // }
+        }
+      }],
+      plotOptions: {
+        series: {
+          marker: {
+            enabled: false,
+            symbol: 'circle'
+          },
+          tooltip: {
+            pointFormatter() {
+              return `<span style="color: #595d78">${this.series.name} at ${nf.format(this.y)}</span>`;
+            }
+          }
+        },
+      },
+      series: series
+    };
+
+    this.setState({ chartOptions: chartOptions })
+  }
+
+  // get the delta on reported cases
+  diff(A) { return A.slice(1).map(function (n, i) { return n - A[i] }) }
+
+  render() {
+    return (
+      <Widget
+        bodyClass="mt"
+        className="mb-xlg"
+        style={{ textTransform: 'capitalize' }}
+        collapse
+        close
+        title={
+          <Row>
+            <Col xs={12} sm={5}>
+              <h6>
+                <span>
+                  {this.state.rollingAvgType} cases since initial {this.state.caseTypeThreshold[this.state.rollingAvgType]}
+                </span>
+              </h6>
+            </Col>
+            <Col xs={12} sm={7}>
+              <div className="chart-legend" />
+            </Col>
+          </Row>
+        }
+      >
+
+        <Row md="12" className="justify-content-center" >
+          <FormGroup size='sm' style={{ marginLeft: '10px' }} className="radio abc-radio">
+            <Input type="radio" id="radio1" name="rollingAvgType" value="confirmed" defaultChecked
+              onChange={(event) => { this.setState({ rollingAvgType: event.target.value }, this.getStatData) }} />
+            <Label for="radio1">Confirmed</Label>
+          </FormGroup>
+          <FormGroup size='sm' style={{ marginLeft: '10px' }} className="radio abc-radio">
+            <Input type="radio" id="radio2" name="rollingAvgType" value="recovered"
+              onChange={(event) => { this.setState({ rollingAvgType: event.target.value }, this.getStatData) }} />
+            <Label for="radio2">Recovered</Label>
+          </FormGroup>
+          <FormGroup size='sm' style={{ marginLeft: '10px' }} className="radio abc-radio">
+            <Input type="radio" id="radio3" name="rollingAvgType" value="deaths"
+              onChange={(event) => { this.setState({ rollingAvgType: event.target.value }, this.getStatData) }} />
+            <Label for="radio3">Death</Label>
+          </FormGroup>
+        </Row>
+        <HighchartsReact
+          ref={this.chartRef}
+          allowChartUpdate={true}
+          options={this.state.chartOptions} />
+      </Widget>
+    );
+  }
+}
---
+++ src/pages/widgets/components/charts/HasCurveFlattenChart.js
@@ -0,0 +1,199 @@
+import React from 'react';
+import { Row, Col } from "reactstrap";
+import Widget from "../../../../components/Widget/Widget";
+import HighchartsReact from 'highcharts-react-official'
+import statsStyles from './ChartStyles'
+import { API } from 'aws-amplify'
+import { ma, } from 'moving-averages'
+const moment = require('moment')
+var nf = new Intl.NumberFormat();
+
+export default class HasCurveFlatten extends React.PureComponent {
+
+
+  constructor(props) {
+    super(props);
+    this.chartRef = React.createRef();
+    this.state = {
+      countryFilter: 'All'
+    }
+  }
+
+  async componentDidMount() {
+
+    // call first time with All
+    await this.getStatData(this.state.countryFilter);
+  }
+
+  /**
+   * analysis done on Active cases and the moving average for trend
+   * @param {*} countryFilter 
+   */
+  async getStatData(countryFilter) {
+    const result = await API.get('covidapi', `/casePoint/totalStat/${countryFilter === 'All' ? '' : countryFilter}`);
+    let series = [];
+
+    // calculate Active cases
+    result.body.forEach(el => { el.active = el.confirmed - el.recovered - el.deaths })
+    let data = result.body.map(el => { return [moment(el.date).utc().format('YYYY-MM-DD'), el.active] })
+
+    data.reverse();
+    // console.log('data', data)
+
+    // calculate Active Delta cases
+    // Bump them to next date on chart
+    let deltaCasePerDay = this.diff(data.map(el => el[1]))
+    deltaCasePerDay.unshift(0)
+    let movingAvg = ma(deltaCasePerDay, 2).flat()
+    // console.log('delta', deltaCasePerDay)
+    // console.log('moving average', movingAvg)
+    let trendUp = (deltaCasePerDay[deltaCasePerDay.length - 1] - movingAvg[movingAvg.length - 1]) >= 0
+    // console.log('trendUp', trendUp)
+
+
+    series.push({
+      name: 'New Active',
+      type: 'column',
+      color: statsStyles['active']['backgroundColorLighter'],
+      data: deltaCasePerDay,
+      tooltip: {
+        valueSuffix: ' mm'
+      }
+    })
+
+    series.push({
+      name: 'Active (MA)',
+      type: 'spline',
+      color: statsStyles['active']['backgroundColor'],
+      data: movingAvg,
+      tooltip: {
+        valueSuffix: ' mm'
+      }
+    })
+
+    // console.log(moment.utc(data[0][0]))
+
+    let chartOptions = {
+      credits: {
+        enabled: false
+      },
+      chart: {
+        height: 350,
+        backgroundColor: 'rgba(0,0,0,0)',
+      },
+      title: false,
+      exporting: {
+        enabled: false
+      },
+      subtitle: {
+        text: 'New Active cases (2-day-average)',
+        style: {
+          color: '#595d78'
+        }
+      },
+      legend: {
+        verticalAlign: 'bottom',
+        itemStyle: {
+          color: "rgba(244, 244, 245, 0.9)"
+        },
+        itemHoverStyle: {
+          color: "rgba(244, 244, 245, 0.6)"
+        }
+      },
+      yAxis: {
+        title: false,
+        min: 0,
+        labels: {
+          style: {
+            color: "#ffffff"
+          }
+        }
+      },
+      xAxis: {
+        type: 'datetime',
+        labels: {
+          overflow: 'justify',
+          style: {
+            color: "#ffffff"
+          }
+        }
+      },
+      annotations: {
+        visible: false
+      },
+      plotOptions: {
+        series: {
+          marker: {
+            enabled: false,
+            symbol: 'circle'
+          },
+          pointStart: moment.utc(data[0][0]).valueOf(),
+          pointInterval: 24 * 3600 * 1000, // one day
+          tooltip: {
+            pointFormatter() {
+              return `<span style="color: #595d78">${this.series.name} at ${nf.format(this.y)}</span>`;
+            }
+          }
+        },
+      },
+      series: series
+    };
+
+    // STUPID
+    if (this.chartRef.current.chart) {
+      // console.log(this.chartRef.current)
+      this.chartRef.current.chart.series.forEach(el =>
+        el.update({
+          pointStart: moment.utc(data[0][0]).valueOf()
+        })
+      )
+
+    }
+
+
+    this.setState({ chartOptions: chartOptions, countryFilter: countryFilter, trendUp: trendUp })
+  }
+
+
+  // get the delta on reported cases
+  diff(A) { return A.slice(1).map(function (n, i) { return n - A[i] }) }
+
+  render() {
+    
+    let trendIcon = <i className="las la-caret-down text-success" style={{ marginTop: '-15px' }}>DOWN</i>
+    
+    if (this.state.trendUp)
+      trendIcon = <i className="las la-caret-up text-danger" style={{ marginTop: '-15px' }}>UP</i>
+
+
+    return (
+      <Widget
+        bodyClass="mt"
+        className="mb-xlg"
+        style={{ textTransform: 'capitalize' }}
+        collapse
+        close
+        title={
+          <Row>
+            <Col xs={12} sm={5}>
+              <h5>
+                <span className="fw-semi-bold">Has the curve flattened?</span>
+              </h5>
+            </Col>
+            <Col xs={12} sm={7}>
+              <div className="chart-legend" />
+            </Col>
+          </Row>
+        }
+      >
+        <div className="d-flex justify-content-right align-items-center mb ">
+          <h4>{this.state.countryFilter}: {trendIcon}</h4>
+        </div>
+        <HighchartsReact
+          ref={this.chartRef}
+          allowChartUpdate={true}
+          options={this.state.chartOptions} />
+      </Widget>
+    );
+  }
+}
